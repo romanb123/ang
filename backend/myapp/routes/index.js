ace777546713d04bf7d421dd8ea1e8fbbbad439d
romanb123 <romanb123@users.noreply.github.com>
2019-10-02 18:21:41 +0300

    ggg

--- backend/myapp/routes/index.js
+++ backend/myapp/routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// get all posts
+router.get('/allposts', function(req, res, next) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    dbo.collection("customers").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      res.send(result);
+      db.close();
+    });
+  });
+});
+
+// get onepost posts
+router.get('/onepost', function(req, res, next) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    var ObjectId = require('mongodb').ObjectID;
+    dbo.collection("customers").find( {"_id":ObjectId("5d94a70040063a1920acb817")} ).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      res.send(result);
+      db.close();
+    });
+  });
 });
 
 module.exports = router;
